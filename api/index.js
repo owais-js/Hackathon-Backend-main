b58a6a0ef79b61871d1952ebb4358ed8104f42c3
Muhammad Owais <145708843+owais-js@users.noreply.github.com>
2025-01-26 08:54:25 +0500

    final

--- api/index.js
+++ api/index.js
@@ -13,14 +13,11 @@ const MONGOURL = process.env.MONGO_URL;
 app.use(express.json());
 
 
-const corsOptions = {
-    origin: ["*"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
-    allowedHeaders: ["Content-Type", "Authorization"], 
-    credentials: true, 
-};
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); 
+app.use(
+    cors({
+        origin:"*"
+})
+)
 
 mongoose
     .connect(MONGOURL)
