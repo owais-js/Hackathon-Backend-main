92efa8503847f9cefaa6cc58dd931a2dcb77b531
Muhammad Owais <145708843+owais-js@users.noreply.github.com>
2025-01-26 08:47:49 +0500

    new

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://hackathon-frontend.netlify.app/"],
+    origin: ["*"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
     allowedHeaders: ["Content-Type", "Authorization"], 
     credentials: true, 
